cc577e347e00b1b010f6cd92c249d59bd49600b6
Zuhdan Nur <pczuhdan33@gmail.com>
2021-06-07 09:18:10 +0700

    fix null

--- app/Modules/Patient/Resources/Views/patient/tabs/treatment.blade.php
+++ app/Modules/Patient/Resources/Views/patient/tabs/treatment.blade.php
@@ -2,16 +2,16 @@
     <ul class="list list-timeline list-timeline-modern pull-t">
         @foreach($patient->treatment as $key => $treatment)
         <li>
-            <div class="list-timeline-time">{{ $treatment->treatment_no }}</div>
+            <div class="list-timeline-time">{{ @$treatment->treatment_no }}</div>
             <i class="list-timeline-icon fa fa-medkit bg-success"></i>
             <div class="list-timeline-content">
-                <p class="font-w600">{{ timezone()->convertToLocal($treatment->date, 'day') }}, {{ timezone()->convertToLocal($treatment->date, 'date') }} | {{ timezone()->convertToLocal($treatment->date, 'time') }}</p>
-                <p>Terapis/Dokter: {{ $treatment->staff->user->full_name }}<br>
-                Ruangan: {{ isset($treatment->room->name) ? $treatment->room->name : '-' }}<br>
-                Status : <span class="badge badge-{{ $treatment->status->class_style }}">{{ $treatment->status->name }}</span>
+                <p class="font-w600">{{ timezone()->convertToLocal(@$treatment->date, 'day') }}, {{ timezone()->convertToLocal(@$treatment->date, 'date') }} | {{ timezone()->convertToLocal(@$treatment->date, 'time') }}</p>
+                <p>Terapis/Dokter: {{ @$treatment->staff->user->full_name }}<br>
+                Ruangan: {{ isset($treatment->room->name) ? @$treatment->room->name : '-' }}<br>
+                Status : <span class="badge badge-{{ @$treatment->status->class_style }}">{{ @$treatment->status->name }}</span>
                  </p>
                 @php
-                    $detail = $treatment->getDetail();
+                    $detail = @$treatment->getDetail();
                     // dd(sizeof($detail));
                 @endphp
                 @if(sizeof($detail) > 0 )
@@ -30,10 +30,10 @@
                                             <span class="text-muted">{{ ($key+1) }}</span>
                                         </td>
                                         <td>
-                                            <span class="text-muted font-w600">{{ $item->name }}</span>
+                                            <span class="text-muted font-w600">{{ @$item->name }}</span>
                                         </td>
                                         <td>
-                                            <span class="text-muted font-w600">{{ $item->description }}</span>
+                                            <span class="text-muted font-w600">{{ @$item->description }}</span>
                                         </td>
                                     </tr>
                                     @endforeach
